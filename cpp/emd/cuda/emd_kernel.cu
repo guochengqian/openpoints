95a5f17a8d5f0ed33d4d3bc302d77f33c29edc0a
Chris Lee <git@clee.ai>
2022-12-05 11:05:38 -0700

    Fix EMD kernel for pytorch 1.13

--- cpp/emd/cuda/emd_kernel.cu
+++ cpp/emd/cuda/emd_kernel.cu
@@ -16,7 +16,9 @@
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
-#define CHECK_EQ(x, y) TORCH_CHECK_EQ(x, y)
+#ifndef CHECK_EQ
+	#define CHECK_EQ(x, y) TORCH_CHECK_EQ(x, y)
+#endif
 
 
 /********************************
